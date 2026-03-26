d42b034f50714f0fa31826781839ec8f250858a4
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2026-03-26 15:28:12 -0300

    chore: add Constants object for network configuration

---
+++ app/src/main/java/com/rodrigo/tmdbexplorer/core/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.rodrigo.tmdbexplorer.core.util
+
+object Constants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "YOUR_API_KEY_HERE"
+    const val LANGUAGE = "en-US"
+}
