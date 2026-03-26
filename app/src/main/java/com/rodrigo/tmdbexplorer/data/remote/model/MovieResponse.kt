54fc084fe851eb93a7b395013bf05a78656878d8
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2026-03-26 15:19:48 -0300

    feat: add MovieResponse wrapper for API results

---
+++ app/src/main/java/com/rodrigo/tmdbexplorer/data/remote/model/MovieResponse.kt
@@ -0,0 +1,14 @@
+package com.rodrigo.tmdbexplorer.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieResponse(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: List<MovieDto>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
